92ad72ee75359a190a027be6edb93528d93b27df
Kimlee Galarneau <kimlee.ny@hotmail.com>
2021-02-23 11:03:08 -0500

    css html

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import './style.css'
+import 'horizontal-scroll/src/';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+
+
+var blocks = document.getElementsByClassName('block');
+var container = document.getElementsByClassName('container');
+var hs = new HorizontalScroll.default({
+  blocks : blocks,
+  container: container,
+});
